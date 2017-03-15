0c22419fbaeb1fcfa8a143d81744aa457d0223b4
MarcosBernal <marcosbernal3@hotmail.com>
2017-03-15 10:32:56 +0100

    Creation of custom_aliases and script to add them

---
+++ installing_aliases.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MY_PATH=$(pwd)/$(dirname $0)
+
+echo 'Adding custom_aliases files to the .bash_aliases file'
+if [ -f $HOME/.bash_aliases ]; then
+  echo -e '\n\n' 				  >> $HOME/.bash_aliases 
+  cat $MY_PATH/custom_aliases >> $HOME/.bash_aliases             
+else
+  cp  $MY_PATH/custom_aliases $HOME/.bash_aliases
+fi   
+
+
+#echo 'Creating .bash_aliases folder in home directory'
+#mkdir -p $HOME/.bash_aliases   
+
+#echo 'Adding custom_aliases files to the .bash_aliases folder'
+#cp $0/../custom_aliases.sh $HOME/.bash_aliases/custom_aliases.sh
+
+#echo 'Adding line to .bashrc file to load custom_aliases'
+#echo -e '# '
+#echo -e 'if [ -f $HOME/.bash_aliases/custom_aliases.sh ]; then' >> $HOME/.bashrc
+#echo -e '  . $HOME/.bash_aliases/custom_aliases.sh'             >> $HOME/.bashrc
+#echo -e 'fi'                                                    >> $HOME/.bashrc
+
+echo 'Setting Complete!!'
